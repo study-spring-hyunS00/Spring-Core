7a39b3a2c0accc59ec43d1f86fd8dfdae86fbe71
hyunS00 <optifun72@gmail.com>
2023-06-27 15:44:57 +0900

    MemberService, OrderService DI로 변경

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -10,8 +10,11 @@ import hello.core.order.OrderServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        OrderService orderService = appConfig.orderService();
+        MemberService memberService = appConfig.memberService();
+//        MemberService memberService = new MemberServiceImpl(memberRepository);
+//        OrderService orderService = new OrderServiceImpl(memberRepository, discountPolicy);
 
         Long memberId = 1L;
         Member member = new Member(memberId,"hyunS00", Grade.VIP);
